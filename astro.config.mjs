9a2fc221eee57eaac8be6b96c6a8789ffef83b63
QuevedoJF <quevedohernandezj126@gmail.com>
2026-04-23 20:41:24 -0600

    Cambio a output static para resolver 404 en Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
 
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
-  integrations: [tailwind()],
-  server: {
-    host: '0.0.0.0',
-    port: 4321
-  }
+  output: 'static',
+  integrations: [tailwind()]
 });
